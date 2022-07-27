3aeb700a3ea141fb502234a31fdfee2f33e50943
Dominik Drozd <dominik_drozd@vp.pl>
2022-07-27 17:01:23 +0200

    Added MySQL database connection and config file.

--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
-import { handleError } from "./utils/error";
+import { handleError } from './utils/error';
+import './utils/db';
+import { config } from './config/config';
 
 const app = express();
 
 app.use(handleError);
 
-app.listen(3000,'127.0.0.1', () => console.log('server is running on port 3000'));
+app.listen(config.port, '127.0.0.1', () =>
+  console.log(`server is running on port ${config.port}`),
+);
--- records/user.record.ts
+++ records/user.record.ts
@@ -1,37 +1,39 @@
 import { v4 as uuid } from 'uuid';
-import {NewUserEntity} from "../types";
-import {AppError} from "../utils/error";
+import { NewUserEntity } from '../types';
+import { AppError } from '../utils/error';
 
 export class UserRecord {
+  private readonly id: string;
+  private name: string;
+  private family: string;
 
-    private readonly id: string;
-    private name: string;
-    private family: string;
+  constructor(obj: NewUserEntity) {
+    this.id = obj.id ?? uuid();
 
-    constructor(obj: NewUserEntity) {
-        this.id = obj.id ?? uuid();
-
-        if(!obj.name || obj.name.length < 2 || obj.name.length > 30) {
-            throw new AppError(`User name must be between 2 and 30 characters - now is ${obj.name.length}.`, 400);
-        }
-
-        if(!obj.family) {
-            throw new AppError('Family must be provided', 400);
-        }
-
-        this.name = obj.name;
-        this.family = obj.family;
+    if (!obj.name || obj.name.length < 2 || obj.name.length > 30) {
+      throw new AppError(
+        `User name must be between 2 and 30 characters - now is ${obj.name.length}.`,
+        400,
+      );
     }
 
-    get userId() {
-        return this.id;
+    if (!obj.family) {
+      throw new AppError('Family must be provided', 400);
     }
 
-    get userName() {
-        return this.name;
-    }
+    this.name = obj.name;
+    this.family = obj.family;
+  }
 
-    get familyName() {
-        return this.family;
-    }
+  get userId() {
+    return this.id;
+  }
+
+  get userName() {
+    return this.name;
+  }
+
+  get familyName() {
+    return this.family;
+  }
 }
