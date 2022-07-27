7a6379b6ff2a5daee209c6bd09fe683f221e25c4
Dominik Drozd <dominik_drozd@vp.pl>
2022-07-27 16:35:48 +0200

    Added custom error class and error handling middleware.

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import 'express-async-errors';
+import { handleError } from "./utils/error";
 
 const app = express();
+
+app.use(handleError);
 
 app.listen(3000,'127.0.0.1', () => console.log('server is running on port 3000'));
---
+++ utils/error.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+
+export class AppError extends Error {
+    constructor(public message: string, public statusCode: number) {
+        super(message);
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+}
+
+export const handleNotFound = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    next(new AppError(`Url: ${req.originalUrl} is not found!`, 404));
+};
+
+export const methodNotAllowed = (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    next(new AppError(`Method ${req.method} is not allowed on this url`, 405));
+};
+
+export const handleError = (
+    err: AppError,
+    req: Request,
+    res: Response,
+    next: NextFunction,
+) => {
+    console.log(err);
+
+    err.statusCode = err.statusCode || 500;
+
+    err.message = err instanceof AppError ? err.message : 'Sorry, please try again later.';
+
+    res.status(err.statusCode).json({
+        message:
+        err.message,
+    });
+
+    next();
+};
